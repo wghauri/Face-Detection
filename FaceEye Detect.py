0281679e1585e7604eb2cc0d37d21e17d3ea3850
wghauri <wghauri04@gmail.com>
2023-01-06 12:03:28 -0600

    first commit

---
+++ FaceEye Detect.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('frontalface_haarcascade.xml')
+eye_cascade = cv2.CascadeClassifier('eye_haarcascade.xml')
+smile_cascade = cv2.CascadeClassifier('smile_haarcascade.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x + w, y + h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex + ew, ey + eh), (0,255,0), 2)
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+        for (sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi_color, (sx,sy), (sx + sw, sy + sh), (0,0,255), 2)
+
+    cv2.imshow('img',img)
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
